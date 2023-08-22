05f8025146a1d2cca6ab89781226e209c68883c7
Kiều Anh Tuấn <tuanka@rocketstudio.com.vn>
2023-08-22 10:55:04 +0700

    initial project

---
+++ MusicPlayer/MusicPlayer/SceneDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  SceneDelegate.swift
+//  MusicPlayer
+//
+//  Created by Tobi on 22/08/2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+
+}
+
